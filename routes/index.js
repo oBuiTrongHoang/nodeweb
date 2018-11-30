a1f292834d51bcef9cf6fa825259adebec884c08
bui.trong.hoang <bui.trong.hoang@framgia.com>
2018-11-30 15:02:00 +0700

    console log

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	console.log('root page');
   res.render('index', {page:'Home', menuId:'home'});
 });
 
